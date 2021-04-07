219f419bae6d35d34391ac4bfcf05574c28bf9f4
Nilstrieb <nils.heydecker@gmx.ch>
2021-04-07 17:06:36 +0200

    nothing working for now

--- bfi-haskell/src/Lib.hs
+++ bfi-haskell/src/Lib.hs
@@ -1,6 +1,29 @@
 module Lib
-    ( someFunc
+    ( main
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Environment
+import Data.List
+import System.IO
+
+main :: IO ()
+main = do
+   args <- getArgs
+   program <- readFile $ head args
+   interpret program
+
+data Memory = Memory [Int] Int [Int]
+
+interpret :: String -> IO ()
+interpret [] = IO()
+interpret [x:xs] = do
+  eval x
+  interpret xs
+
+
+eval :: String -> Memory -> IO Memory
+eval s (Memory sx x xs)
+| x == "+" = Memory sx (x + 1) xs
+| x == "-" = Memory sx (x - 1) xs
+otherwise = undefined
+eval _ = undefined
