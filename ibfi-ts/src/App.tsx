b30fa04dccf43788fffebf76d29b361c4ba3dea5
Nilstrieb <nilstrieb@gmail.com>
2021-06-23 14:55:43 +0200

    oops ts

---
+++ ibfi-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App-header">
+            <div className="bf-input">
+                <textarea placeholder="Input your code here..."/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
