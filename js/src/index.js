54b5c9b1f87bd625d0f2d95c755ea95122975134
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-05-16 12:25:31 +0200

    javascript

---
+++ js/src/index.js
@@ -0,0 +1,110 @@
+import fs from 'fs/promises';
+
+function lex(string) {
+  const tokens = [];
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    if (['+', '-', '>', '<', '.', ',', '[', ']'].includes(char)) {
+      tokens.push({
+        char,
+        span: i
+      });
+    }
+  }
+
+  return tokens;
+}
+
+function ParseError(message, span) {
+  this.message = message;
+  this.span = span;
+}
+
+function Parser(tokens) {
+  this.tokens = tokens;
+  this.position = 0;
+}
+
+Parser.prototype.next = function() {
+  const token = this.tokens[this.position];
+  this.position++;
+  return token;
+};
+
+Parser.prototype.parse = function(isLoop) {
+  const body = [];
+  let nextToken;
+  while ((nextToken = this.next()) !== undefined) {
+    switch (nextToken.char) {
+      case '[': {
+        const loopBody = this.parse(true);
+        body.push(loopBody);
+        break;
+      }
+      case ']': {
+        if (isLoop) {
+          return body;
+        } else {
+          throw new ParseError('No matching `[` found', nextToken.span);
+        }
+      }
+      default: {
+        body.push(nextToken);
+      }
+    }
+  }
+
+  if (isLoop) {
+    throw new ParseError('No matching `]` found', this.tokens[this.tokens.length - 1].span);
+  } else {
+    return body;
+  }
+};
+
+function reportError(source, message, span) {
+  let lineIdx = 0;
+  let lastNewlineIdx = 0;
+  for (let i = 0; i < source.length; i++) {
+    const char = source[i];
+    if (i === span) {
+      break;
+    }
+    if (char === '\n') {
+      lineIdx++;
+      lastNewlineIdx = i;
+    }
+  }
+
+  const lines = source.split('\n');
+  const line = lines[lineIdx];
+  const lineNumber = String(lineIdx + 1);
+
+  const linePrefix = `${lineNumber} | `;
+  const lineSpan = span - lastNewlineIdx;
+
+  console.error(`error: ${message}`);
+  console.error(`${linePrefix}${line}`);
+  console.error(`${' '.repeat(linePrefix.length)}${'-'.repeat(lineSpan)}^`);
+}
+
+const file = process.argv[2];
+
+if (!file) {
+  console.error('Usage: [filename]');
+  process.exit(1);
+}
+
+const source = await fs.readFile(file, 'utf-8');
+const tokens = lex(source);
+
+const parser = new Parser(tokens);
+try {
+  const ast = parser.parse(false);
+  console.log(ast);
+} catch (parseError) {
+  if (!(parseError instanceof ParseError)) {
+    throw parseError;
+  }
+  reportError(source, parseError.message, parseError.span);
+}
